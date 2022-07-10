e8b199ecc6e11a8423d769bf4bf949281abbfbb1
ada0l <andreika.varfolomeev@yandex.ru>
2022-07-10 14:06:25 +1000

    update

--- backend/backend/photo/api/v1.py
+++ backend/backend/photo/api/v1.py
@@ -35,26 +35,25 @@ async def get_photos(
 async def upload_photos(
     user_id: str = Form(...),
     user_to_id: str = Form(...),
-    text: str | None = Form(default=None),
-    file: str | None = Form(default=None),
+    text: str | None = Form(None),
+    file: str | None = Form(None),
     user_rep: UserRepository = Depends(get_user_repository),
     photo_rep: PhotoRepository = Depends(get_photo_repository),
 ):
     user_in = UserInCreatePydantic(id=user_id)
     user_to_in = UserInCreatePydantic(id=user_to_id)
 
-    response = s3_client.get_object(
-        Bucket="vk-mini-apps", Key="alembic_version.png"
-    )
     file_name = None
-    if file is not None:
+    if file:
         try:
-            file = file[len("data:image/png;base64,"):]
+            file = file[len("data:image/png;base64,") :]
             image_as_bytes = str.encode(file)
             img_recovered = base64.b64decode(image_as_bytes)
-            file_name = str(uuid.uuid4())
+            file_name = uuid.uuid4()
             s3_client.put_object(
-                Body=img_recovered, Bucket="vk-mini-apps", Key=f"{file_name}.png"
+                Body=img_recovered,
+                Bucket="vk-mini-apps",
+                Key=f"{file_name}.png",
             )
         except Exception as e:
             print(e)
@@ -63,7 +62,7 @@ async def upload_photos(
     user_to = await user_rep.get_or_create(user_to_in)
     photo = await photo_rep.create_with_user(
         PhotoInCreatePydantic(
-            uuid_str=file_name,
+            uuid_str=f"{file_name}",
             user_id=user.id,
             user_to_id=user_to.id,
             text=text,
